40b5fe466d82b23b658a345bd001f984c7457626
TaegyunHa <taegyunha92@gmail.com>
2018-11-10 11:39:28 +0000

    Making the predcition

--- rnn_ha.py
+++ rnn_ha.py
@@ -65,8 +65,22 @@ regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
 # 3. Making the predictions and visualising the result
 
 # Getting the real stock price of 2017
+dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
+real_stock_price = dataset_test.iloc[:,1:2].values 
 
 # Getting the prediction stock price of 2017
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+# Creating a data structure for each line of observation to predict next stock price
+X_test = []
+for i in range(60, 80):
+  X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
 # Visualising the result
 
